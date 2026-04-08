40b808c87e06b484be854ecd058eea9b494de61b
Dominika Maziec <dominika_maziec@hms.harvard.edu>
2026-04-08 15:52:34 -0400

    updates

---
+++ 1.truthset_illumina/Mutect2/scripts/sentieon_TNhaplotyper2_wOrientationBias_ContaminationModel_normal.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+# *******************************************
+# Script to run TNhaplotyper2 on tumor-only data.
+# Generate OrientationBias and ContaminationModel metrics.
+# Implemented to run in distributed mode using shards.
+# *******************************************
+
+## Functions
+generate_random_string() {
+    echo "$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 5 | head -n 1)"
+}
+
+process_bam_header() {
+    # Variable to store replacement @RG args
+    replace_rg_args=""
+
+    # Extract @RG lines from the header of the input BAM
+    input_bam="$1"
+    sample_name="$2"
+    rg_lines=$(samtools view -H "$input_bam" | grep "^@RG")
+
+    # Loop through @RG lines and modify ID field
+    # Create --replace_rg arguments
+    while IFS= read -r rg_line; do
+        OLD_SAMPLE_NAME=$(echo $rg_line | grep -o 'SM:[^ ]*' | cut -d':' -f2)
+        orig_rg_id=$(echo "$rg_line" | awk -F'\t' '/ID:/ {print $2}' | sed "s/ID://")
+        new_rg_id="${orig_rg_id}-$(generate_random_string)"
+        new_rg_line=$(echo "$rg_line" | cut -f 2-)
+        new_rg_line=$(echo "$new_rg_line" | sed "s/\(SM:\)[^\\t]*/\1${sample_name}/")
+        formatted_new_rg_line=$(echo -e "$new_rg_line" | sed 's/\t/\\t/g')
+        replace_rg_args+=" --replace_rg ${orig_rg_id}='${formatted_new_rg_line}' "
+    done <<< "$rg_lines"
+
+    # Return replacement @RG args
+    echo "$replace_rg_args"
+}
+
+## Parse command-line arguments
+while getopts ":s:i:r:p:t:b:n:m:" opt; do
+  case $opt in
+    s) shards_file="$OPTARG"
+    ;;
+    i) shard_index="$OPTARG"
+    ;;
+    r) genome_reference_fasta="$OPTARG"
+    ;;
+    p) population_allele_frequencies="$OPTARG"
+    ;;
+    t) sample_name="$OPTARG"
+    ;;
+    b) sample_bams="$OPTARG"
+    ;;
+    n) normal_name="$OPTARG"
+    ;;
+    m) normal_bams="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+done
+
+# Other settings
+nt=$SLURM_CPUS_PER_TASK # number of threads to use in computation
+
+# ******************************************
+# 1. Process BAM headers and prepare input files
+# ******************************************
+input_files=""
+
+# Add processed sample BAMs
+for sample_bam in ${sample_bams[*]}; do
+    replace_args=$(process_bam_header "$sample_bam" "$sample_name")
+    input_files+=" $replace_args -i $sample_bam "
+done
+
+# Add processed normal BAMs
+for normal_bam in ${normal_bams[*]}; do
+    replace_args=$(process_bam_header "$normal_bam" "$normal_name")
+    input_files+=" $replace_args -i $normal_bam "
+done
+
+# ******************************************
+# 2. Extract regions from shard
+# ******************************************
+grep -P "\@${shard_index}\t" "$shards_file" | cut -f 2 > SHARDS_LIST
+
+regions=""
+while read -r line; do
+    regions+=" --shard $line"
+done <SHARDS_LIST
+
+# ******************************************
+# 3. Construct TNhaplotyper2 command line
+# ******************************************
+command="sentieon driver -t $nt -r $genome_reference_fasta $input_files $regions"
+command+=" --algo TNhaplotyper2 --tumor_sample $sample_name \
+    --normal_sample $normal_name \
+    --germline_vcf $population_allele_frequencies \
+    output.vcf.gz"
+command+=" --algo OrientationBias --tumor_sample $sample_name \
+    output.priors"
+command+=" --algo ContaminationModel --tumor_sample $sample_name \
+    --normal_sample $normal_name \
+    -v $population_allele_frequencies \
+    output.contamination"
+
+# ******************************************
+# 4. Run TNhaplotyper2 command
+# ******************************************
+ulimit -s 65536 # Argument list can potentially be too long otherwise (default is 8192)
+eval $command || exit 1
---
+++ 1.truthset_illumina/Mutect2/scripts/submit_TNFilter_normal.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#------
+
+
+module load gcc
+module load samtools
+
+# Input
+# Variables
+FOLDER=$1
+SAMPLE_NAME=$2
+NORMAL_NAME=$3
+FOLDER_OUTPUT=$4
+SHARDS_LEN=$5
+
+
+# The script take as input a file with a list of BAM files
+# Read the input file and store the content in a string
+FILES=""
+cd $FOLDER_OUTPUT/$FOLDER
+
+vcfs=$(bash /n/data1/hms/dbmi/park/dominika/input_files.sh $FOLDER_OUTPUT/$FOLDER output.vcf.gz $SHARDS_LEN)
+priors=$(bash /n/data1/hms/dbmi/park/dominika/input_files.sh $FOLDER_OUTPUT/$FOLDER output.priors $SHARDS_LEN)
+contamination=$(bash /n/data1/hms/dbmi/park/dominika/input_files.sh $FOLDER_OUTPUT/$FOLDER output.contamination $SHARDS_LEN)
+
+# Run reference sample files 
+bash /n/data1/hms/dbmi/park/dominika/testing/smaht/tissues/scripts/sentieon_merge_TNfilter_normal.sh $REFERENCE $SAMPLE_NAME $NORMAL_NAME $vcfs $priors $contamination
